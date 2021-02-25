9f8d47636f8ebffd874f80526cfc985d1c075a31
AshleyHall <ahall1@jmc.edu.au>
2021-02-26 09:57:33 +1100

    Add base Unity project
    - Added project to GitHub
    - Added simple player movement
    - Added simple environment

---
+++ Roll-A-Ball/Assets/Scripts/GoodbyeWorld.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodbyeWorld : MonoBehaviour
+{
+    // sETTING UP BASIC PLAYER STATS
+    public int playerHealth = 100;
+    public float gameTime = 0.5f;
+    public string welcomeMessage = "Oh no, not roll a ball again"; 
+    public bool playerDead = false;
+
+    void Start()
+    {
+        //CHECK IF PALYER IS DEAD
+        if (playerHealth > 0)
+        {
+            print(welcomeMessage);
+            playerDead = false;
+        }
+        else if (playerHealth == 0)
+        {
+            print("Just dead");
+        }
+        else
+        {
+            print("Get good scrub");
+            playerDead = true;
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+    void Objection()
+    {
+        print("MOSHI MOSHI");
+        if(0 == 0)
+        {
+            print("cool");
+        }
+
+    }
+
+}
